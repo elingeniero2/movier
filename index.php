bcee9a494e029a3df61bea62a87056df7f9f0861
JUAN C RUIZ R <elingeniero_2@hotmail.com>
2017-10-25 02:19:18 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="source/foundation/foundation-6.4.2/css/foundation.css">
+    <link rel="stylesheet" href="source/foundation/foundation-6.4.2/css/app.css">
+</head>
+    <body>
+
+  
+  <!-- <div class="row"> -->
+    <div class="fullscreen-image-slider">
+      <div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
+        <ul class="orbit-container">
+          <!-- <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
+          <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button> -->
+          <li class="is-active orbit-slide">
+            <img class="orbit-image" src="//foundation.zurb.com/sites/docs/assets/img/orbit/01.jpg" alt="Space">
+            <figcaption class="orbit-caption">Space, the final frontier.</figcaption>
+          </li>
+          <li class="orbit-slide">
+            <img class="orbit-image" src="//foundation.zurb.com/sites/docs/assets/img/orbit/02.jpg" alt="Space">
+            <figcaption class="orbit-caption">Lets Rocket!</figcaption>
+          </li>
+          <li class="orbit-slide">
+            <img class="orbit-image" src="//foundation.zurb.com/sites/docs/assets/img/orbit/03.jpg" alt="Space">
+            <figcaption class="orbit-caption">Encapsulating</figcaption>
+          </li>
+          <li class="orbit-slide">
+            <img class="orbit-image" src="//foundation.zurb.com/sites/docs/assets/img/orbit/04.jpg" alt="Space">
+            <figcaption class="orbit-caption">Outta This World</figcaption>
+          </li>
+        </ul>
+        <!-- <nav class="orbit-bullets">
+          <button class="is-active" data-slide="0"><span class="show-for-sr">First slide details.</span><span class="show-for-sr">Current Slide</span></button>
+          <button data-slide="1"><span class="show-for-sr">Second slide details.</span></button>
+          <button data-slide="2"><span class="show-for-sr">Third slide details.</span></button>
+          <button data-slide="3"><span class="show-for-sr">Fourth slide details.</span></button>
+        </nav> -->
+      </div>
+    </div>
+  <!-- </div> -->
+  
+        <script src="source/foundation/foundation-6.4.2/js/vendor/jquery.js"></script>
+        <script src="source/foundation/foundation-6.4.2/js/vendor/what-input.js"></script>
+        <script src="source/foundation/foundation-6.4.2/js/vendor/foundation.js"></script>
+        <script src="source/foundation/foundation-6.4.2/js/app.js"></script>
+    </body>
+</html>
